2cce0c058ecb824ed8be58b23e5fb195845691ef
Pavlov Danil <danpavkzn@mail.ru>
2024-06-23 21:21:01 +0300

    end_videcourse

--- test.py
+++ test.py
@@ -3,6 +3,10 @@ import sqlite3 as sq
 with sq.connect("saper.db") as con:
     cur = con.cursor()
 
-    cur.execute("SELECT * FROM users WHERE score > 100 ORDER BY score LIMIT 5")
+    # cur.execute("""CREATE TABLE tab1(
+    # score INTEGER,
+    # from TEXT
+    # )""")
+    cur.execute("SELECT * FROM users")
     result = cur.fetchall()
     print(result)
